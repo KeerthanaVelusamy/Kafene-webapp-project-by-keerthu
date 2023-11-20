26506333741bd8f51b680c6d7587f3bcd44332c5
KeerthanaVelusamy <148248808+KeerthanaVelusamy@users.noreply.github.com>
2023-11-20 12:42:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+let loginCredentials = localStorage.getItem("loginStatus");
+if(loginCredentials == "false" || loginCredentials == null){
+
+}else{
+    location.assign("./orders.html");
+}
+
+var userName = document.getElementsByClassName(".loginusername");
+var userPassword = document.getElementsByClassName(".loginpassword");
+let submitBtn = document.getElementById("submit");
+let form = $("#login-form");
+form.submit(function(e){
+    e.preventDefault();
+
+    const name = userName.value;
+    const password = userPassword.value;
+    var mObj = {
+      username: name,
+      password: password,
+    };
+    // // Checking the Username and password provided by user to meet the required condition -----
+    if (name === password && name !== "" && password !== "") {
+        alert("Login‌ ‌Successful");
+        localStorage.setItem("loginStatus", JSON.stringify(mObj));
+        location.assign("./orders.html");
+    }
+    else {
+        alert("Please‌‌ enter‌ ‌valid‌ ‌credentials!");
+    }
+})
+
+document.getElementById("sign-out").style.display = "none";
